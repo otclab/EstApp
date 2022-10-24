fa49a4fe84613c9fe621da35063c80da869b3357
oscar-alpha <otc.lab@gmail.com>
2022-10-24 00:49:50 -0500

    Se agrga la capacidad de leer y escribir los umbrales desde una hoja de calculo Excel.

--- cmds/thresholdMgmt.py
+++ cmds/thresholdMgmt.py
@@ -2,6 +2,9 @@
 #-*- coding: utf-8 -*-
 
 import sys
+from dataclasses import dataclass
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Alignment, NamedStyle, Font, Border, Side
 from common import openCard
 
 
@@ -10,7 +13,7 @@ def ThresholdCmd(args, port, throughput_limit) :
     EstApp : Umbrales de Tensión de los Taps
     ===========================================
 
-    Tiene cuatro formas de invocarlo :
+    Tiene cinco formas de invocarlo :
       (a) Presenta una tabla con la lista de los taps activos :
 
           >> EstApp.py -u
@@ -36,15 +39,28 @@ def ThresholdCmd(args, port, throughput_limit) :
           valor del umbral de cambio al siguiente tap e 'inf' o'inferior' para
           el valor del umbral al tap anterior.
 
-      (d) Modifica los umbrales definidos en un archivo de una hoja de calculo
+       (d) Modifica los umbrales definidos en un archivo de una hoja de calculo
           Excel.
 
-          >> EstApp.py -u Nombre_del_archivo
+          >> EstApp.py -u -r Nombre_del_archivo
 
-          Si el nombre del archivo contiene espacios deben incluirse
-          comillas (dobles) al inicio y final. Los umbrales de cada tap
-          se definen a partir de la segunda fila, en la segunda columna (B) el
-          umbral superior y en la tercera (C) el inferior. :
+          El nombre del archivo debe incluir la ruta, si es que no se encuentra
+          en el directorio actual. Si contiene espacios deberá incluirse entre
+          comillas (dobles). En la hoja de Excel los umbrales de cada tap se
+          definen a partir de la segunda fila, en la segunda columna (B) el
+          umbral superior y en la tercera (C) el inferior.
+
+
+       (e) Lee los umbrales definidos en la tarjeta y los guarda en una hoja de
+           calculo Excel.
+
+          >> EstApp.py -u -w Nombre_del_archivo
+
+          El nombre del archivo debe incluir la ruta, si es que no se encuentra
+          en el directorio actual. Si contiene espacios deberá incluirse entre
+          comillas (dobles). En la hoja de Excel los umbrales de cada tap se
+          definen a partir de la segunda fila, en la segunda columna (B) el
+          umbral superior y en la tercera (C) el inferior.
 
 
   """
@@ -113,12 +129,132 @@ def ThresholdCmd(args, port, throughput_limit) :
                    pos[pos.index(args[3][:3])+1].capitalize(), args[2],
                                getattr(card.threshold[tap_idx], args[3][:3])))
 
+  elif args[2] == '-w' :
+    print(f'args : {args}')
+    if len(args) > 3 :
+      write_xls(card, args[3])
+
+    else :
+      print(f'Error : Falta el nombre de la hoja Excel.')
+      card.close()
+      sys.exit(1)
+
+  elif args[2] == '-r' :
+    if len(args) > 3 :
+      read_xls(card, args[3])
+
+    else :
+      print(f'Error : Falta el nombre de la hoja Excel.')
+      card.close()
+      sys.exit(1)
+
+
   else :
-    print(u'Error : "%s" no es el índice de un tap o '                      \
-                                          u'el sub-comando "total".' %args[2])
+    print(f'Error : {args[2]} no es el sub-comando "total", "r" o "w" ni '    \
+                                                      f'el índice de un tap.')
     print('Pruebe la opción -h -u para ver los detalles.')
     card.close()
     sys.exit(1)
 
   card.close()
+
+
+@dataclass
+class Threshold:
+    sup : float
+    inf : float
+
+    def __post_init__(self) :
+        if self.sup is None or self.inf is None :
+            raise ValueError(f'Se trato de asignar un umbral incompleto.')
+
+class ThresholdList(list) :
+    def __init__(self, filename = None) :
+        wb = load_workbook(filename = filename)
+        ws = wb.active
+
+        # Se validan los umbrales
+        super().__init__()
+        for n, row in enumerate(ws[2:7]) :
+            sup, inf  = row[1].value, row[2].value
+
+            if sup is None and inf is None :
+                break
+
+            if sup is None :
+                raise ValueError(f'Falta definir el umbral superior del tap {n+1}.')
+
+            if not isinstance(sup, (int, float)) :
+                raise ValueError(f'El umbral superior del tap {n+1} no es un número.')
+
+            if inf is None :
+                raise ValueError(f'Falta definir el umbral inferior del tap {n+1}.')
+
+            if not isinstance(inf, (int, float)) :
+                raise ValueError(f'El umbral inferior del tap { n+1} no es un número.')
+
+            if sup <= inf :
+                raise ValueError('Los umbrales del tap {n+1} estan invertidos.')
+
+            if super().__len__() > 1 :
+                if self[-1].sup < inf :
+                    msg = f'Los taps {n} y {n+1} no se traslapan.\n' +                                \
+                          f'(El umbral superior del tap {n} es menor que el inferior del tap {n+1})'
+                    raise ValueError(msg)
+
+            super().append(Threshold(sup, inf))
+
+
+def read_xls(card, xls_name) :
+  try :
+    threshold_list = ThresholdList(filename = xls_name)
+  except ValueError as e :
+    print(f'Error : {e.args[0]}')
+    card.close()
+    sys.exit(1)
+
+  for n, th in enumerate(threshold_list) :
+     setattr(card.threshold[n], 'sup', th.sup)
+     setattr(card.threshold[n], 'inf', th.inf)
+
+  card.threshold.len = len(threshold_list)
+
+  print(f'Los umbrales se guardaron en \'{xls_name}\'')
+
+
+def write_xls(card, xls_name) :
+  accounting_format = '_ * #,##0.00_ ;_ * \\-#,##0.00_ ;_ * "-"??_ ;_ @_ '
+  thin  = Side(border_style='thin', color='000000')
+  thick = Side(border_style='thick', color='000000')
+
+  wb = Workbook()
+  ws = wb.active
+
+  ws.title = 'Asignación de Taps'
+  ws['A1'], ws['B1'], ws['C1'] = '# Tap', 'Umbral\nAlto', 'Umbral\nBajo'
+
+  for c in 'A1 B1 C1'.split() :
+    ws[c].alignment = Alignment(wrapText='True',
+                                      horizontal='center', vertical='center')
+
+  for c in 'A1 B1 C1'.split() :
+    ws[c].border = Border(top=thick, left=thick, right=thick, bottom=thick)
+
+
+  for n, th in enumerate(card.threshold[:card.threshold.len]) :
+    ws[f'A{n+2}'], ws[f'B{n+2}'], ws[f'C{n+2}'] = n+1, th.sup, th.inf
+    ws[f'A{n+2}'].alignment = Alignment(wrapText='True',
+                                      horizontal='center', vertical='center')
+    ws[f'B{n+2}'].number_format = accounting_format
+    ws[f'C{n+2}'].number_format = accounting_format
+
+    bottom = thick if n == (card.threshold.len - 1) else thin
+    ws[f'A{n+2}'].border = Border(top=thin , left=thick, right=thick, bottom=bottom)
+    ws[f'B{n+2}'].border = Border(top=thin , left=thick, right=thin , bottom=bottom)
+    ws[f'C{n+2}'].border = Border(top=thin , left=thin , right=thick, bottom=bottom)
+
+
+
+  wb.save(xls_name)
+
 
